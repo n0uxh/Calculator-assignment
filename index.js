860332b1da56a7c911bdd746e1a0920d022eb2e3
Nousheen Arshad <n.sheikharshad@gmail.com>
2024-03-29 16:25:15 -0700

    Calculator

---
+++ index.js
@@ -0,0 +1,36 @@
+import inquirer from "inquirer";
+const answers = await inquirer.prompt([
+    {
+        name: 'firstNumber',
+        message: 'Enter first num:',
+        type: 'number',
+    }, {
+        name: 'secondNumber',
+        message: 'Enter second num:',
+        type: 'number',
+    }, {
+        name: 'operator',
+        message: 'Select Operator:',
+        type: 'list',
+        choices: ['Add', 'Subtract', 'Multiply', 'Divide']
+    }
+]);
+console.log(answers);
+let result;
+if (answers.operator === 'Add') {
+    //Addition function
+    result = answers.firstNumber + answers.secondNumber;
+}
+else if (answers.operator === 'Subtract') {
+    //Subtraction function
+    result = answers.firstNumber - answers.secondNumber;
+}
+else if (answers.operator === 'Multiply') {
+    //Multiply
+    result = answers.firstNumber * answers.secondNumber;
+}
+else {
+    //Divide
+    result = answers.firstNumber / answers.secondNumber;
+}
+console.log(`Result is ${result}`);
